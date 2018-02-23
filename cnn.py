bc361f967f7bbe99c260d5362e25a5ac7a49f47e
Aditya Gune <adi.vtca@gmail.com>
2018-02-22 18:43:31 -0800

    Initial commit

---
+++ cnn.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 18:41:57 2018
+
+@author: adivt
+"""
+
+import torch 
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
